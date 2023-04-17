e5beb6ece1754a285b8bab72ad10eec5141a590b
alyssa liu <40530927+alysssssa@users.noreply.github.com>
2023-04-17 01:12:51 +0100

    add colour.h

--- raytracer/main.cpp
+++ raytracer/main.cpp
@@ -1,3 +1,7 @@
+
+#include "colour.h"
+#include "vec3.h"
+
 #include <iostream>
 
 int main() {
@@ -16,15 +20,8 @@ int main() {
         // write progress indicator to the error output stream
         std::cerr << "\rScanlines remaining: " << j << " " << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.25;
-
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            std::cout << ir << " " << ig << " " << ib << "\n";
+            colour pixel_colour(double(i)/(image_width-1), double(j)/(image_height-1), 0.25);
+            write_colour(std::cout, pixel_colour);
         }
     }
         std::cerr << "\nDone.\n";

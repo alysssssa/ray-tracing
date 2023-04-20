4c12f1399f50296744b4ca3d07490cf700536af0
alyssa liu <40530927+alysssssa@users.noreply.github.com>
2023-04-20 01:02:42 +0100

    sending rays into the scene

--- raytracer/main.cpp
+++ raytracer/main.cpp
@@ -1,15 +1,41 @@
 
 #include "colour.h"
 #include "vec3.h"
+#include "ray.h"
 
 #include <iostream>
+
+// ray tracer:
+// 1. calculate the ray from the eye to the pixel
+// 2. determine which objects the ray intersects
+// 3. compute a colour for that intersection point
+
+// return colour of the background
+colour ray_colour(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    double t = 0.5*(unit_direction.y() + 1.0);
+    // linear interpolation between white (t=0) and blue (t=1)
+    // blended_value = (1-t)*start_value + t*end_value
+    return (1.0-t)*colour(1.0, 1.0, 1.0) + t*colour(0.5, 0.7, 1.0);
+}
 
 int main() {
 
     // write output image in ppm
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
 
-    const int image_width = 256;
-    const int image_height = 256;
+    // camera
+    // x - right, y - up, z - out of the screen
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
 
     // ppm format:
     // P3 - colours in ASCII; column number; row number; 255 - for max colour;
@@ -18,9 +44,12 @@ int main() {
 
     for (int j = image_height-1; j >= 0; --j) {
         // write progress indicator to the error output stream
-        std::cerr << "\rScanlines remaining: " << j << " " << std::flush;
+        std::cerr << "\rScanline remaining: " << j << " " << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            colour pixel_colour(double(i)/(image_width-1), double(j)/(image_height-1), 0.25);
+            auto u = double(i) / (image_width-1);
+            auto v = double(j) / (image_height-1);
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+            colour pixel_colour = ray_colour(r);
             write_colour(std::cout, pixel_colour);
         }
     }

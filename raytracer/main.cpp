bed3e077a3b6988f85abfa98fdd9bbe81630e826
alyssa liu <40530927+alysssssa@users.noreply.github.com>
2023-04-23 00:00:43 +0100

    antialiasing

--- raytracer/main.cpp
+++ raytracer/main.cpp
@@ -3,6 +3,7 @@
 #include "colour.h"
 #include "hittable_list.h"
 #include "sphere.h"
+#include "camera.h"
 
 #include <iostream>
 
@@ -34,6 +35,7 @@ int main() {
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
 
     // world
     hittable_list world;
@@ -41,15 +43,7 @@ int main() {
     world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
     // camera
-    // x - right, y - up, z - out of the screen
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
-
-    auto origin = point3(0, 0, 0);
-    auto horizontal = vec3(viewport_width, 0, 0);
-    auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+    camera cam;
 
     // ppm format:
     // P3 - colours in ASCII; column number; row number; 255 - for max colour;
@@ -60,11 +54,14 @@ int main() {
         // write progress indicator to the error output stream
         std::cerr << "\rScanline remaining: " << j << " " << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto u = double(i) / (image_width-1);
-            auto v = double(j) / (image_height-1);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-            colour pixel_colour = ray_colour(r, world);
-            write_colour(std::cout, pixel_colour);
+            colour pixel_colour(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; ++s) {
+                auto u = (i + random_double()) / (image_width-1);
+                auto v = (j + random_double()) / (image_height-1);
+                ray r = cam.get_ray(u, v);
+                pixel_colour += ray_colour(r, world);
+            }
+            write_colour(std::cout, pixel_colour, samples_per_pixel);
         }
     }
         std::cerr << "\nDone.\n";
--- raytracer/raytracer.h
+++ raytracer/raytracer.h
@@ -5,6 +5,8 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <cstdlib>
+//#include <random>
 
 using std::shared_ptr;
 using std::make_shared;
@@ -17,6 +19,30 @@ const double pi = 3.1415926535897932385;
 // Utility Functions
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+    // random number in [0,1)
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    // random number in [min,max)
+    return min + (max-min)*random_double();
+}
+
+// alternative random number generator
+//inline double random_double2() {
+//    // random number in [0,1)
+//    static std::uniform_real_distribution<double> distribution(0.0,1.0);
+//    static std::mt19937 generator;
+//    return distribution(generator);
+//}
+
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
 }
 
 // Common Headers

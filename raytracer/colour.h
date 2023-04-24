9b8892ff3cf759642b41cfbda506db20cf063168
alyssa liu <40530927+alysssssa@users.noreply.github.com>
2023-04-24 17:41:00 +0100

    diffuse sphere with gamma correction

--- raytracer/colour.h
+++ raytracer/colour.h
@@ -11,10 +11,13 @@ void write_colour(std::ostream &out, colour pixel_colour, int samples_per_pixel)
     auto b = pixel_colour.z();
 
     // divide the colour by the number of samples
+    // apply gamma correction of 2.0 (raise colour to the power 1/gamma)
+    // gamma correction is used to "undo" the gamma correction applied to the image by the display
+    // because almost all image viewers assume the image is gamma corrected before being stored as a byte
     auto scale = 1.0 / samples_per_pixel;
-    r *= scale;
-    g *= scale;
-    b *= scale;
+    r = sqrt(scale * r);
+    g = sqrt(scale * g);
+    b = sqrt(scale * b);
 
     // write the translated [0, 255] value of each colour component
     out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << " "

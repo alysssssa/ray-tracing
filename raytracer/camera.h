52ff547ce98c0643aea49953749591a055850126
alyssa liu <40530927+alysssssa@users.noreply.github.com>
2023-04-30 16:43:07 +0100

    positionable and orientable camera

--- raytracer/camera.h
+++ raytracer/camera.h
@@ -7,6 +7,9 @@
 class camera {
     public:
         camera(
+                point3 lookfrom, // camera origin
+                point3 lookat, // camera target
+                vec3 vup, // view up
                 double vfov, // vertical field-of-view in degrees
                 double aspect_ratio
             ) {
@@ -16,16 +19,21 @@ class camera {
             auto viewport_height = 2.0 * h;
             auto viewport_width = aspect_ratio * viewport_height;
 
-            auto focal_length = 1.0;
+            // w - camera's forward direction
+            auto w = unit_vector(lookfrom - lookat);
+            // u - camera's right direction
+            auto u = unit_vector(cross(vup, w));
+            // v - camera's up direction
+            auto v = cross(w, u);
 
-            origin = point3(0, 0, 0);
-            horizontal = vec3(viewport_width, 0, 0);
-            vertical = vec3(0, viewport_height, 0);
-            lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+            origin = lookfrom;
+            horizontal = viewport_width * u;
+            vertical = viewport_height * v;
+            lower_left_corner = origin - horizontal/2 - vertical/2 - w;
         }
 
-        ray get_ray(double u, double v) const {
-            return ray(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+        ray get_ray(double s, double t) const {
+            return ray(origin, lower_left_corner + s*horizontal + t*vertical - origin);
         }
 
     private:

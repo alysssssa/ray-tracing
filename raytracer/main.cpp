e3b5ec10c01dcd0ed8122d57f0f29a96eaeca408
alysssssa <alyssa.h.liu@gmail.com>
2023-05-04 17:03:43 +0100

    checkered spheres

--- raytracer/main.cpp
+++ raytracer/main.cpp
@@ -90,6 +90,17 @@ hittable_list random_scene() {
     return {make_shared<bvh_node>(world, 0.0, 1.0)};
 }
 
+hittable_list two_spheres() {
+    hittable_list objects;
+
+    auto checker = make_shared<checker_texture>(colour(0.2, 0.3, 0.1), colour(0.9, 0.9, 0.9));
+
+    objects.add(make_shared<sphere>(point3(0,-10,0), 10, make_shared<lambertian>(checker)));
+    objects.add(make_shared<sphere>(point3(0,10,0), 10, make_shared<lambertian>(checker)));
+
+    return objects;
+}
+
 int main() {
 
     // write output image in ppm
@@ -100,16 +111,36 @@ int main() {
     const int max_depth = 50;
 
     // world
-    auto world = random_scene();
+    hittable_list world;
+
+    point3 lookfrom;
+    point3 lookat;
+    auto vfov = 40.0;
+    auto aperture = 0.0;
+
+    switch (0) {
+        case 1:
+            world = random_scene();
+            lookfrom = point3(13,2,3);
+            lookat = point3(0,0,0);
+            vfov = 20.0;
+            aperture = 0.1;
+            break;
+
+        default:
+        case 2:
+            world = two_spheres();
+            lookfrom = point3(13,2,3);
+            lookat = point3(0,0,0);
+            vfov = 20.0;
+            break;
+    }
 
     // camera with depth of field
-    point3 lookfrom(13,2,3);
-    point3 lookat(0,0,0);
     vec3 vup(0,1,0);
     auto dist_to_focus = 10.0;
-    auto aperture = 0.1;
 
-    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
+    camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
 
     // ppm format:
     // P3 - colours in ASCII; column number; row number; 255 - for max colour;

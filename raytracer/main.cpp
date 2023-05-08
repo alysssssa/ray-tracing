e4af3e24746c96334c68772319eb6d29c4c53fc2
alyssa liu <40530927+alysssssa@users.noreply.github.com>
2023-05-08 13:49:42 +0100

    earth-mapped sphere

--- raytracer/main.cpp
+++ raytracer/main.cpp
@@ -111,6 +111,14 @@ hittable_list two_perlin_spheres() {
     return objects;
 }
 
+hittable_list earth() {
+    auto earth_texture = make_shared<image_texture>("external/earthmap.jpg");
+    auto earth_surface = make_shared<lambertian>(earth_texture);
+    auto globe = make_shared<sphere>(point3(0,0,0), 2, earth_surface);
+
+    return {globe};
+}
+
 int main() {
 
     // write output image in ppm
@@ -144,13 +152,21 @@ int main() {
             vfov = 20.0;
             break;
 
-        default:
         case 3:
             world = two_perlin_spheres();
             lookfrom = point3(13,2,3);
             lookat = point3(0,0,0);
             vfov = 20.0;
             break;
+
+        default:
+        case 4:
+            world = earth();
+            lookfrom = point3(13,2,3);
+            lookat = point3(0,0,0);
+            vfov = 20.0;
+            break;
+
     }
 
     // camera with depth of field

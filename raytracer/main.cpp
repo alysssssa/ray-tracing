835791a7ac2fd68d8d7232d6457db68129dcad95
alysssssa <alyssa.h.liu@gmail.com>
2023-05-10 17:09:07 +0100

    cornell box with glass

--- raytracer/main.cpp
+++ raytracer/main.cpp
@@ -232,12 +232,41 @@ hittable_list popcorn_box() {
     return objects;
 }
 
+hittable_list cornell_glass() {
+    hittable_list objects;
+
+    auto red = make_shared<lambertian>(colour(.65, .05, .05));
+    auto white = make_shared<lambertian>(colour(.73, .73, .73));
+    auto green = make_shared<lambertian>(colour(.12, .45, .15));
+    auto light = make_shared<diffuse_light>(colour(5, 5, 5));
+    auto glass = make_shared<dielectric>(1.5);
+
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 555, green));
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red));
+    objects.add(make_shared<xz_rect>(113, 443, 127, 432, 554, light));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
+    objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
+
+    shared_ptr<hittable> box1 = make_shared<box>(point3(0,0,0), point3(165,330,165), glass);
+    box1 = make_shared<rotate_y>(box1, 15);
+    box1 = make_shared<translate>(box1, vec3(265,0,295));
+    objects.add(box1);
+
+    shared_ptr<hittable> box2 = make_shared<box>(point3(0,0,0), point3(165,165,165), glass);
+    box2 = make_shared<rotate_y>(box2, -18);
+    box2 = make_shared<translate>(box2, vec3(130,0,65));
+    objects.add(box2);
+
+    return objects;
+}
+
 int main() {
 
     // write output image in ppm
     const auto aspect_ratio = 1.0;
     // const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 600;
+    const int image_width = 1000;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 50;
     const int max_depth = 50;
@@ -309,12 +338,20 @@ int main() {
             vfov = 40.0;
             break;
 
-        default:
         case 8:
             world = popcorn_box();
             background = colour(0,0,0);
             lookfrom = point3(278, 278, -800);
             lookat = point3(278, 240, 0);
+            vfov = 40.0;
+            break;
+
+        default:
+        case 9:
+            world = cornell_glass();
+            background = colour(0,0,0);
+            lookfrom = point3(278, 278, -800);
+            lookat = point3(278, 278, 0);
             vfov = 40.0;
             break;
     }
